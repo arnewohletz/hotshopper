422de5166ce1d5250aaf18957ee375f7e2c09b86
horsewithnoname1985 <arnewohletz@gmx.de>
2021-10-21 14:44:43 +0200

    remove unnecessary functions

--- hotshopper/static/hotshopper.js
+++ hotshopper/static/hotshopper.js
@@ -2,15 +2,6 @@ function init() {
     window.scroll(0, scroll_height);
 }
 
-// function shopping_list_calc() {
-//     const recipes_table = document.getElementById("recipe_selection")
-//     // for (let i = 0; i < recipes_table.rows.length; i++) {
-//     //     var item = recipes_table.innerText.
-//     // }
-//         console.log(recipes_table.innerHTML.id);
-//
-//     var recipeSelection = JSON.stringify("#recipe_selection");
-// }
 
 function set_selected(checkboxElem) {
     let scroll_height = document.documentElement.scrollTop || document.body.scrollTop
@@ -22,12 +13,5 @@ function set_selected(checkboxElem) {
     }
 }
 
-// function hide_shopping_list() {
-//     document.getElementById("shopping_list_table").style.display = "none";
-// }
-//
-// function show_shopping_list() {
-//     document.getElementById("shopping_list_table").style.display = "block";
-// }
 
 window.onload = init;
