d3212b72727cc3ba9052b2c7a9484daab5fdd5c4
horsewithnoname1985 <arnewohletz@gmx.de>
2021-10-21 15:10:49 +0200

    remove not needed commented out stuff

--- hotshopper-runner.py
+++ hotshopper-runner.py
@@ -1,13 +1,5 @@
 from hotshopper.hotshopper import main
 
-import random
 
 if __name__ == "__main__":
     main(web=True)
-    # port = 5001
-    # port = 5000 + random.randint(0, 999)
-    # url = "http://127.0.0.1"
-
-    # if autostart:
-    #     threading.Timer(1.25, lambda: webbrowser.open(url)).start()
-    # main().run(port=port, debug=True)
--- hotshopper/hotshopper.py
+++ hotshopper/hotshopper.py
@@ -1,18 +1,11 @@
 """Main module."""
-from hotshopper.recipes import Recipe
-from hotshopper.ui import View
-
-from hotshopper.foodplan import FoodPlan
-
-import random
-import threading
-import webbrowser
 import secrets
 
+from flask import (Flask, render_template, redirect, session)
 
-from flask import (Flask, render_template, request, redirect, make_response,
-                   session
-                   )
+from hotshopper.foodplan import FoodPlan
+from hotshopper.recipes import Recipe
+from hotshopper.ui import View
 
 
 class Controller:
@@ -34,27 +27,9 @@ class Controller:
         self.view.display_shopping_lists(shopping_lists)
 
 
-# class Controller:
-#     def __init__(self, view=None):
-#         self.foodplan = None
-#         self.recipes = []
-#         if view:
-#             self.view = view
-#
-#     def get_recipes(self):
-#         self.recipes = [recipe() for recipe in Recipe.__subclasses__()]
-#         return sorted(self.recipes, key=lambda recipe: recipe.name)
-
-
 def main(web=True):
-    app = None
     controller = Controller()
     recipes = controller.get_recipes()
-
-    # Controller(app)
-    # view = View()
-    # Controller(view)
-    # view.mainloop()
 
     if web:
         app = Flask(__name__)
@@ -100,4 +75,3 @@ def main(web=True):
         view = View()
         Controller(view)
         view.mainloop()
-
