f8274ca5eeb1780cea5f29d5a702fcf3b5be0fde
horsewithnoname1985 <arnewohletz@gmx.de>
2022-05-03 11:42:10 +0200

    Add more custom errors

--- hotshopper/errors.py
+++ hotshopper/errors.py
@@ -1,3 +1,13 @@
 class UnsupportedUnitError(Exception):
     def __init__(self, *args, **kwargs):
         pass
+
+
+class DuplicateRecipeIngredientError(Exception):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class RecipeIngredientNotFoundError(Exception):
+    def __init__(self, *args, **kwargs):
+        pass
