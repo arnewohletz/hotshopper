f2cdfbbca1d6836ac4ee61dd1c2f9907bdc5b8e2
horsewithnoname1985 <arnewohletz@gmx.de>
2023-07-06 13:02:09 +0200

    bump version to 1.0.0-rc

--- hotshopper/__init__.py
+++ hotshopper/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Arne Wohletz"
 __email__ = "arnewohletz@gmx.de"
-__version__ = "0.2.0"
+__version__ = "1.0.0-rc"
 
 import secrets
 from pathlib import Path
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/horsewithnoname1985/hotshopper',
-    version='0.2.0',
+    version='1.0.0-rc',
     zip_safe=False,
 )
