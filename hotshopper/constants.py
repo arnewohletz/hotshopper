97592e4bda99ed149e60b7abfc11d5390cb45ac2
horsewithnoname1985 <arnewohletz@gmx.de>
2024-04-09 18:44:20 +0200

    Don't use constants.json for now

--- hotshopper/constants.py
+++ hotshopper/constants.py
@@ -9,13 +9,17 @@ location = json.load(open(json_file))['Location']
 unit = json.load(open(json_file))['Unit']
 
 
-@dataclass
-class Location:
-    SUPERMARKET = location['SUPERMARKET']
-    MARKET = location['MARKET']
+# @dataclass
+# class Location:
+#     # SUPERMARKET = location['SUPERMARKET']
+#     # MARKET = location['MARKET']
+#     SUPERMARKET = "supermarket"
+#     MARKET = "market"
 
 
 @dataclass
 class Unit:
-    GRAM = unit['GRAM']
-    PIECE = unit['PIECE']
+    # GRAM = unit['GRAM']
+    # PIECE = unit['PIECE']
+    GRAM = "g"
+    PIECE = "St."
