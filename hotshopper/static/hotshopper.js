e5b038bc55d741994e35d26a9d3f25afa69f2657
horsewithnoname1985 <arnewohletz@gmx.de>
2024-09-12 18:24:00 +0200

    Improve layout on main screen

--- hotshopper/static/hotshopper.js
+++ hotshopper/static/hotshopper.js
@@ -29,6 +29,10 @@ function show_ingredients_screen() {
     window.location.href = "/ingredients"
 }
 
+function show_print_preview() {
+    window.location.href = "/print_shopping_list"
+}
+
 function show_shopping_list_screen() {
     window.location.href = "/shopping_list/0"
 }
